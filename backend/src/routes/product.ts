60987b27a0fbd0539be53f3584516f210c09ee7f
spiritualbless <yaroslav.svakhin@yandex.ru>
2025-12-17 23:42:03 +0900

    добавление валидации при создании заказа

--- backend/src/routes/product.ts
+++ backend/src/routes/product.ts
@@ -1,9 +1,10 @@
 import { createProduct, getProduct } from "../controllers/product";
 import { Router } from "express";
+import { productValidation } from "../middlewares/validators";
 
 const router = Router();
 
 router.get('/product', getProduct);
-router.post('/product', createProduct);
+router.post('/product', productValidation, createProduct);
 
 export default router
